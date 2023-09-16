0b777e66437b00a2106eba94c737289294b0fca8
Tuấn Trần <tuantransn2001@gmail.com>
2023-09-16 19:56:37 +0700

    fix db connect string

--- src/api/v1/database/database.provider.ts
+++ src/api/v1/database/database.provider.ts
@@ -1,14 +1,16 @@
 import * as mongoose from 'mongoose';
 import { PROVIDER_NAME } from '../ts/enums/common';
-// const DB_CONNECT_LINK =
-//   process.env.NODE_ENV === 'production'
-//     ? process.env.DB_CONNECT_LINK_PROD
-//     : process.env.DB_CONNECT_LINK_DEV;
+
+const DB_CONNECT_LINK =
+  process.env.NODE_ENV === 'production'
+    ? process.env.DB_CONNECT_LINK_PROD
+    : process.env.DB_CONNECT_LINK_DEV;
+
 export const databaseProviders = [
   {
     provide: PROVIDER_NAME.DATABASE_CONNECTION,
     useFactory: (): Promise<typeof mongoose> => {
-      return mongoose.connect(`${process.env.DB_CONNECT_LINK_DEV}`);
+      return mongoose.connect(`${DB_CONNECT_LINK}`);
     },
   },
 ];
