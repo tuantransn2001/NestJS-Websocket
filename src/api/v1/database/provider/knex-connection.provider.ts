0f13ef548ec741a9eced7971f8606d4184a67b7e
Tuấn Trần <tuantransn2001@gmail.com>
2024-01-24 21:47:54 +0700

    change project name,hide db_connection_string

--- src/api/v1/database/provider/knex-connection.provider.ts
+++ src/api/v1/database/provider/knex-connection.provider.ts
@@ -1,30 +1,23 @@
+import * as dotenv from 'dotenv';
 import { knex } from 'knex';
-import { knexSnakeCaseMappers, Model } from 'objection';
+import { Model } from 'objection';
 import { ProviderName } from '../../common/enums/common';
+
+dotenv.config();
 
 export const databaseProviders = [
   {
     provide: ProviderName.KNEX_CONNECTION,
 
     useFactory: () => {
-      // creates a Knex connection
-      // https://knexjs.org
       const knexConn = knex({
         client: 'pg',
-        connection:
-          'postgresql://postgres:tuantransn2001@localhost:5432/messenger',
+        connection: process.env.POSTGRESQL_DB_CONNECT_LINK,
         migrations: {
           directory: './src/api/v1/database/knex/migrations',
           extension: 'ts',
           loadExtensions: [`.ts`],
-          // copy-paste migrations
-          // stub: './src/database/stubs/migration.stub',
         },
-        //   seeds: {
-        //     directory: './src/database/seeds',
-        //     extension: extension,
-        //     loadExtensions: [`.${extension}`],
-        //   },
       });
 
       // now every model has this knex instance
@@ -37,27 +30,3 @@ export const databaseProviders = [
     },
   },
 ];
-// export const databaseProviders = [
-//   {
-//     provide: ProviderName.KNEX_CONNECTION,
-//     useFactory: () => {
-//       const isDevEnv = process.env.NODE_ENV === 'develop';
-//       const database_connection_string = process.env.POSTGRESQL_DB_CONNECT_LINK;
-
-//       const knexConn = knex({
-//         client: 'pg',
-//         connection: {
-//           connectionString: database_connection_string,
-//         },
-//         pool: { min: 0, max: 100 },
-//         debug: isDevEnv,
-
-//         ...knexSnakeCaseMappers(),
-//       });
-
-//       Model.knex(knexConn);
-
-//       return knexConn;
-//     },
-//   },
-// ];
